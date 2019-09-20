e1a8482e477b07f138833647b923ec78e2f87658
Phil Shaw <poadshaw@gmail.com>
2019-09-19 20:06:21 -0500

    20190919_2006

--- bin/userFreshInstall_RUN_2nd.sh
+++ bin/userFreshInstall_RUN_2nd.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-
-# Make downloads folder
-mkdir -p ${HOME}/downloads
-cd  ${HOME}/downloads
+# update all packages
+sudo pacman -Syu
 
 # Set up git
+cd  ${HOME}
 git config --global user.name "Phil Shaw"
 git config --global user.email "poadshaw@gmail.com"
+
+# Make downloads folder if it does not already exist
+[ ! -d "%{HOME}/downloads"  ] && mkdir -p ${HOME}/downloads
+cd  ${HOME}/downloads
 
 # Install polybar
 git clone https://aur.archlinux.org/polybar.git
@@ -20,8 +23,10 @@ curl -fLo ~/.config/polybar/config https://raw.githubusercontent.com/poshaw/arch
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-python -m pip install --user pynvim
+# Install Python extensions
+cd  ${HOME}
+python -m pip install --user --upgrade pynvim
 
 
-sudo mount /dev/sdb1 /mnt/usb -o uid=${USER},gid=${USER},fmask=113,dmask=002
-cd /mnt/usb/arch
+# sudo mount /dev/sdb1 /mnt/usb -o uid=${USER},gid=${USER},fmask=113,dmask=002
+# cd /mnt/usb/arch
