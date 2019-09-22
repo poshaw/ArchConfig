18f7c40f9bdce5aa0920a2def9b9e79ddde66a5e
Phil Shaw <poadshaw@gmail.com>
2019-09-22 11:45:19 -0500

    20190922_1145

--- bin/i3listen.py
+++ bin/i3listen.py
@@ -1,15 +1,18 @@
 #! /usr/bin/python3
+
 from i3ipc import Connection
 from subprocess import call
 
 i3 = Connection()
 
 def windownotify(i3, event):
-    if event.container.fullscreen_mode == 0:
-        call('polybar-msg cmd show'.split(' '))
-    else:
-        call('polybar-msg cmd hide'.split(' '))
-        # print(event.container.fullscreen_mode)
+	if event.container.fullscreen_mode == 0:
+		call('polybar-msg cmd show'.split(' '))
+	else:
+		call('polybar-msg cmd hide'.split(' '))
+		# print(event.container.fullscreen_mode)
+	if event.change in "focus" "title":
+		call('polybar-msg hook titlehook 2'.split(' '))
 
 i3.on('window', windownotify)
 
